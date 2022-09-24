cb6b36eb87e0b7c51ee142a63bdd2f16839845f0
yamashin0922 <y@zenryokukikai.com>
2022-09-24 13:16:26 +0900

    Creating 3D maps if you set the folder which includes .png and .txt files

--- plot_map.py
+++ plot_map.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+#plt.rcParams['font.family'] = 'IPAexGothic'
 from matplotlib import animation
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
@@ -7,6 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
 matplotlib.use('TKAgg')
 
+
 import sys
 import pandas as pd
 from scipy.interpolate import griddata
@@ -14,6 +16,19 @@ import argparse
 #import terrain_csv
 import math
 import cv2
+import os
+
+#print( plt.rcParams['font.family'] )
+
+from matplotlib.font_manager import FontProperties
+
+
+
+font_path = '/usr/share/fonts/opentype/ipaexfont-gothic/ipaexg.ttf'
+font_prop = FontProperties(fname=font_path)
+matplotlib.rcParams['font.family'] = font_prop.get_name()
+
+import glob
 
 def read_csv(file_name):
     df = pd.read_csv(file_name)
@@ -25,33 +40,10 @@ class PlotMap:
         self.ax0 = self.fig.add_subplot(121, projection='3d')
         self.ax1 = self.fig.add_subplot(122, aspect='equal')
 
-    def rotate_3d_surface(self, angle):
-        self.ax0.view_init(azim=angle)
-        self.ax1.view_init(azim=angle)
+    def plot_map(self, title, img):
+        self.fig.suptitle(title)
 
-    def plot_contour(self, ax, x, y, z, cmap=cm.gist_earth):
-        ax.contourf(x, y, z, cmap=cmap)
 
-    def plot_3d_surface(self, ax, title, x, y, z, z_max, z_min, cmap=cm.gist_earth):
-
-        surf = ax.plot_surface(x, y, z, cmap=cmap, linewidth=0, antialiased=False)
-        ax.set_xlim(np.nanmin(x), np.nanmax(x))
-        ax.set_ylim(np.nanmin(y), np.nanmax(y))
-        ax.set_zlim(z_min, z_max)
-        ax.set_title(title)
-        ax.zaxis.set_major_locator(LinearLocator(6))
-        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-    def plot_3d_trisurf(self, ax, title, x, y, z, z_max, z_min, cmap=cm.gist_earth):
-        surf = ax.plot_trisurf(list(x), list(y), list(z), cmap=cmap, linewidth=0, antialiased=False)
-        ax.set_xlim(np.nanmin(x), np.nanmax(x))
-        ax.set_ylim(np.nanmin(y), np.nanmax(y))
-        ax.set_zlim(z_min, z_max)
-        ax.set_title(title)
-        ax.zaxis.set_major_locator(LinearLocator(6))
-        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-    def plot_map(self, title, img, is_show = False, animated = False):
         rows,cols = img.shape
         x = np.array(range(0, cols))
         y = np.array(range(0, rows))
@@ -62,28 +54,46 @@ class PlotMap:
         self.ax0.plot_surface(X, Y, z, cmap=cm.gist_earth, linewidth=0, antialiased=False)
         self.ax1.contourf(X, Y, z, cmap=cm.gist_earth)
 
-        if animated:
-            rot_animation = animation.FuncAnimation(self.fig, self.rotate_3d_surface, frames=np.arange(0,362,2),interval=100)
-            rot_animation.save('soil.gif', dpi=80, writer='imagemagick')
-        #print('Save fig. as', title + '.png')
-        plt.savefig(title + '.png', dpi=120)
+        plt.savefig(output_filepath + '_3d.png', dpi=120)
 
-        if is_show:
-            plt.show()
-        return
-
-
-    def save_as_file(self, title):
-        plt.savefig(title + '.jpg', dpi=120)
-
+def read_text_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='parameters')
-    parser.add_argument('-png_file', dest='png_file', type=str, help='png file path', required=True)
+    parser.add_argument('-dataset', dest='dataset', type=str, help='dataset dir', required=True)
+    parser.add_argument('-output', dest='output', type=str, help='output dir', required=True)
+    #parser.add_argument('-basefilename', dest='basefilename', type=str, help='dataset dir', required=True)
     params = parser.parse_args()
 
-    png_file = params.png_file
-    img = cv2.imread(png_file, cv2.IMREAD_ANYDEPTH)
+    dataset = params.dataset
+    #basefilename = params.basefilename
+    output = params.output
 
-    p = PlotMap().plot_map('Test_PlotMap', img, is_show = True, animated = False)
+    #print(f"{dataset}/*.txt")
+    #txt_files = glob.glob(f"{dataset}/*.txt")
+    #print('txt_files',txt_files)
 
+    for path in glob.glob(f"{dataset}/*.txt"):
+        print('path',path)
+        # check if current path is a file
+        if os.path.isfile(path):
+            #res.append(path)
+
+            basefilename = os.path.splitext(os.path.basename(f"{dataset}{path}"))[0]
+            #print('basefilename',basefilename)
+
+            output_filepath = os.path.join(output, basefilename)
+            filepath = os.path.join(dataset, basefilename)
+            png_file = filepath + '.png'
+            txt_file = filepath + '.txt'
+            title = read_text_file(txt_file)
+   
+            img = cv2.imread(png_file, cv2.IMREAD_ANYDEPTH)
+
+            p = PlotMap().plot_map(title, img)
+
+            is_show = False
+            if is_show:
+                plt.show()
